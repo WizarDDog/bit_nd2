fb3bc6cc19648d55a3e38ac9fea86e7322b4f4ff
root <root@localhost.localdomain>
2018-09-26 18:16:05 +0300

    first commit

---
+++ src/body.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+
+
+class Body extends Component {
+  render() {
+    return (
+      <div className="pilnas">
+      	<div className="kaire divas">
+      		links
+      		<a>asdasd</a>
+      		<a>asdasd</a>
+      		<a>asdasd</a>
+      	</div>
+      	<p className="vidurys divas">Lorem Ipsum is simply dummy text 
+      	of the printing and typesetting industry </p>
+      	<div className="desine divas">
+      		links
+      		<a>asdasd</a>
+      		<a>asdasd</a>
+      		<a>asdasd</a>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+export default Body;
